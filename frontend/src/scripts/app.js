797780a8b2671476e1bd6a6ca3f20d54840a43f4
Hisun0 <tridvad1@gmail.com>
2023-06-25 21:08:11 +0300

    now api is working

--- frontend/src/scripts/app.js
+++ frontend/src/scripts/app.js
@@ -1,11 +1,11 @@
 const searchButton = document.querySelector('.image-button');
 
-function search() {
+async function search() {
   const cityName = document.querySelector('.search-input').value;
   const apiKey = 'c8fb17bee74c48f0a0a8d6d542831ded';
   const apiCall = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
 
-  fetch(apiCall)
+  await fetch(apiCall)
     .then(response => response.json())
     .then(
       result => {
