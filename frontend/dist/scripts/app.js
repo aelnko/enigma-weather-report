a26bc4fa851a83dab8db23cc4eecb36afd8079de
aelnko <lekolupaeva1201@gmail.com>
2023-06-25 20:49:49 +0300

    connect api

--- frontend/dist/scripts/app.js
+++ frontend/dist/scripts/app.js
@@ -1,1 +1,1 @@
-const hello=()=>console.log("Hello, World!");
+const searchButton=document.querySelector(".image-button");function search(){const e=document.querySelector(".search-input").value;fetch(`https://api.openweathermap.org/data/2.5/weather?q=${e}&appid=c8fb17bee74c48f0a0a8d6d542831ded`).then((e=>e.json())).then((e=>{console.log(e)}))}searchButton.addEventListener("click",search);
--- frontend/src/scripts/app.js
+++ frontend/src/scripts/app.js
@@ -1,1 +1,17 @@
-const hello = () => console.log('Hello, World!');
+const searchButton = document.querySelector('.image-button');
+
+function search() {
+  const cityName = document.querySelector('.search-input').value;
+  const apiKey = 'c8fb17bee74c48f0a0a8d6d542831ded';
+  const apiCall = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+
+  fetch(apiCall)
+    .then(response => response.json())
+    .then(
+      result => {
+        console.log(result);
+      }
+    ) 
+}
+
+searchButton.addEventListener('click', search);
