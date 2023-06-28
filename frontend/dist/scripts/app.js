c78c0a34e5f97bd78c787d30a1e433c904242d7f
Hisun0 <tridvad1@gmail.com>
2023-06-28 14:23:40 +0300

    add dark and light theme

--- frontend/dist/scripts/app.js
+++ frontend/dist/scripts/app.js
@@ -1,1 +1,1 @@
-import{renderWeatherInfo}from"./utils/render.js";const app=()=>{document.querySelector(".image-button").addEventListener("click",renderWeatherInfo)};document.querySelector(".image-button").addEventListener("click",renderWeatherInfo);
+import{renderWeatherInfo}from"./utils/render.js";import switchTheme from"./utils/theme.js";const app=()=>{const e=document.querySelector(".image-button"),t=document.querySelector(".theme");e.addEventListener("click",renderWeatherInfo),t.addEventListener("click",switchTheme)};app();
---
+++ frontend/src/scripts/utils/theme.js
@@ -0,0 +1,41 @@
+const switchTheme = () => {
+  const currentTheme = document.body.id;
+  const themes = ['light', 'dark'];
+  const colors = {
+    light: '#ffffff',
+    dark: '#231f01',
+    blue: '#5195fa',
+  };
+
+  const getNextTheme = (theme) => {
+    const currentIndex = themes.indexOf(theme);
+    const nextIndex = (currentIndex + 1) % themes.length;
+    return themes[nextIndex];
+  };
+
+  const reverse = (theme) => {
+    const headerDiv = document.querySelector('.header');
+    const switchButtons = headerDiv.querySelectorAll('button');
+    const images = headerDiv.querySelectorAll('img');
+
+    switchButtons.forEach((button) => {
+      button.style.backgroundColor = colors[currentTheme];
+      if (button.classList.contains('registration-button')) {
+        button.style.backgroundColor = colors.blue;
+      }
+    });
+
+    images.forEach((image) => {
+      const [, , , , fileName] = image.src.split('/');
+      image.src = `./image/${fileName.replace(currentTheme, theme)}`;
+    });
+
+    document.body.style.backgroundColor = colors[theme];
+    document.body.id = theme;
+  };
+
+  const nextTheme = getNextTheme(currentTheme);
+  reverse(nextTheme);
+};
+
+export default switchTheme;
